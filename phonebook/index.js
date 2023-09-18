fbcb484a7f1fa174f7ee9177b09f9d24336978e3
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2023-09-18 16:30:47 -0700

    add cors

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const morgan = require('morgan')
 
@@ -35,6 +36,7 @@ const generateId = () => {
     return maxId + 1
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :raw'))
 
